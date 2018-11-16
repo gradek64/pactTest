b183a6c86f9cfb6619f5d4168c1fd993945be832
GILGRZ <gradek64@yahoo.com>
2018-11-16 17:33:16 +0000

    init

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+const dataStore = {
+  count: 1000
+}
+
+router.get('/provider', (req, res) => {
+  const validDate = req.query.validDate
+  //const dateRegex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}/
+
+  if (!validDate) {
+    res.status(400)
+    res.json({ error: 'validDate is required' });
+  } else if (!moment(validDate, moment.ISO_8601).isValid()) {
+    res.status(400)
+    res.json({ error: `'${validDate}' is not a date` })
+  } else {
+    if (dataStore.count > 0) {
+      res.json({
+        'test': 'NO',
+        'validDate': moment(new Date(), moment.ISO_8601).format('YYYY-MM-DDTHH:mm:ssZ'),
+        'count': dataStore.count
+      })
+    } else {
+      res.status(404)
+      res.send()
+    }
+  }
+})
+
+module.exports = router;
