50246d35ce7f405f79264440c2d65b20143fcbb9
GILGRZ <gradek64@yahoo.com>
2018-12-04 16:36:38 +0000

    latest sever1

--- server.js
+++ server.js
@@ -16,18 +16,18 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // set the home page route
-app.get('/', function(req, res) {
+app.get('/html', function(req, res) {
   // U should specify header but if not browser will adjust
   res.header('Content-Type', 'text/html; charset=utf-8')
 	// ejs render automatically looks in the views folder
-	res.render('index');
-  //res.end('hi');
+	//res.render('index');
+  res.end('hi');
 });
 
 app.get('/json', (req, res) => {
     //specifif header only for this route
     res.header('Content-Type', 'application/json; charset=utf-8')
-    res.json({json:'it is json response object'})
+    res.json({json:'it is json response object set'})
 })
 
 app.listen(port, function() {
