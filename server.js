7562dbfb0ffaab0a1ef0de8e9210103eab354fad
GILGRZ <gradek64@yahoo.com>
2018-12-04 17:11:22 +0000

    latest

--- server.js
+++ server.js
@@ -10,18 +10,38 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // set the home page route
 app.get('/', function(req, res) {
-  // U should specify header but if not browser will adjust
   res.header('Content-Type', 'text/html; charset=utf-8')
 	// ejs render automatically looks in the views folder
 	res.render('index');
   //res.end('hi');
+});
+
+app.get('/getPDF/:download', (req, res) => {
+  var filename = req.params.download;
+  res.setHeader('Content-type', 'application/pdf');
+  const viewsPath = path.join(__dirname,'views');
+  var file = viewsPath + `/${filename}`;
+  res.download(file,filename);
+});
+
+app.get('/login', (req, res) => {
+  const login = req.query.login;
+  res.header('Content-Type', 'application/json; charset=utf-8');
+
+  if (!login) {
+    res.status(400);
+    res.json({ error: 'login is required' });
+  } else {
+    res.json({
+      login: login,
+    });
+  }
 });
 
 app.get('/json', (req, res) => {
